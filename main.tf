837f6bd16cba4c22213b3e0032efe89945d35459
GitHub Action <action@github.com>
2021-08-18 18:57:40 +0000

    Terraform GitHub Action

--- main.tf
+++ main.tf
@@ -10,12 +10,12 @@ resource "random_string" "username" {
 }
 
 resource "random_password" "password" {
-  length      = 32
-  min_lower   = 1
-  min_upper   = 1
-  min_numeric = 1
-  min_special = 1
-  special     = true
+  length           = 32
+  min_lower        = 1
+  min_upper        = 1
+  min_numeric      = 1
+  min_special      = 1
+  special          = true
   override_special = "!@#$%*()-_=+[]{}:?"
 }
 
