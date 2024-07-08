5289912607082c9692657b5e22bb66921cd96931
Wayne Carty <wayne.carty@lexisnexis.com>
2024-07-08 09:48:12 -0400

    Update main.tf
    Updating accelerated_networking resource

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "azurerm_network_interface" "dynamic" {
   location                      = var.names.location
   resource_group_name           = var.resource_group_name
   tags                          = var.tags
-  enable_accelerated_networking = var.accelerated_networking
+  accelerated_networking_enabled = var.accelerated_networking
 
   ip_configuration {
     name                          = "internal"
